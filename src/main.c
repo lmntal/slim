a29b76ae70ba8e68d57fca28778c44dbc9505d42
Taichi Tomioka <mys.itachi@gmail.com>
2016-10-04 02:54:46 +0900

    Move files to loader.

--- src/main.c
+++ src/main.c
@@ -44,7 +44,7 @@
 #include "symbol.h"
 #include "vm/vm.h"
 #include "loader/load.h"
-#include "translate.h"
+#include "loader/translate.h"
 #include "arch.h"
 #include "lmntal_system_adapter.h"
 #include "verifier/verifier.h"
