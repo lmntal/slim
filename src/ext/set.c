e559705006dd7c7b592adad613fec5e2a8ed4a87
Yutaro TSUNEKAWA <tsunekawa@ueda.info.waseda.ac.jp>
2017-05-03 14:49:32 +0900

    Add cb_set_insert.

--- src/ext/set.c
+++ src/ext/set.c
@@ -10,6 +10,12 @@
 static int set_atom_type; /* special atom type */
 
 /**
+ * @memberof LmnSet
+ * @private
+ */
+#define LMN_SET_DATA(obj) (LMN_SET(obj)->tbl)
+
+/**
  * @brief Internal Constructor
  * @memberof LmnSet
  * @private
@@ -20,6 +26,43 @@ static LmnSetRef make_id_set(LmnMembraneRef mem)
   LMN_SP_ATOM_SET_TYPE(s, set_atom_type);
   s->tbl = st_init_table(&type_id_hash);
   return s;
+}
+/*----------------------------------------------------------------------
+ * Callbacks
+ */
+
+/*
+ * 挿入
+ *
+ * +a0: 集合
+ * +a1: 要素
+ * -a2: 新集合
+ */
+/**
+ * @memberof LmnSet
+ * @private
+ */
+void cb_set_insert(LmnReactCxtRef rc,
+		   LmnMembraneRef mem,
+		   LmnAtomRef a0, LmnLinkAttr t0,
+		   LmnAtomRef a1, LmnLinkAttr t1,
+		   LmnAtomRef a2, LmnLinkAttr t2)
+{
+  if(LMN_INT_ATTR == t1) {	/* id set */
+    if(t0 != LMN_SP_ATOM_ATTR) {
+      LmnSetRef s = make_id_set(mem);
+      LmnLinkAttr attr = LMN_SP_ATOM_ATTR;
+      lmn_mem_push_atom(mem, LMN_ATOM(s), attr);
+      lmn_mem_delete_atom(mem, a0, t0);
+      a0 = LMN_ATOM(s);
+      t0 = attr;
+    }
+    st_insert(LMN_SET_DATA(a0), (st_data_t)a1, (st_data_t)a1);
+    lmn_mem_delete_atom(mem, a1, t1);
+  }
+  lmn_mem_newlink(mem,
+                  a0, t0, LMN_ATTR_GET_VALUE(t0),
+                  a2, t2, LMN_ATTR_GET_VALUE(t2));
 }
 
 /*----------------------------------------------------------------------
@@ -83,5 +126,7 @@ void init_set(void)
                                        sp_cb_set_eq,
                                        sp_cb_set_dump,
                                        sp_cb_set_is_ground);
+
+  lmn_register_c_fun("cb_set_insert", (void *)cb_set_insert, 3);
 }
 
