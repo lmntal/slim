4fa784859d9be947fcc243a311972b0d7af21d6d
Nozomi MATSUZAWA <matsuzawa@ueda.info.waseda.ac.jp>
2017-05-03 15:26:24 +0900

    Add cb_mem_equals.

--- src/ext/react_rule.c
+++ src/ext/react_rule.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "vm/vm.h"
+#include "hash.h"
 
 void cb_react_rule(LmnReactCxtRef rc,
 			  LmnMembraneRef mem,
@@ -134,9 +135,48 @@ void cb_mhash(LmnReactCxtRef rc,
 		  mem_proxy, mem_proxy_link_attr, LMN_ATTR_GET_VALUE(mem_proxy_link_attr));
 }
 
+void cb_mem_equals(LmnReactCxtRef rc,
+		   LmnMembraneRef mem,
+		   LmnAtom mem0_proxy, LmnLinkAttr mem0_proxy_link_attr,
+		   LmnAtom mem1_proxy, LmnLinkAttr mem1_proxy_link_attr,
+		   LmnAtom ret_mem0_link, LmnLinkAttr ret_mem0_link_attr,
+		   LmnAtom ret_mem1_link, LmnLinkAttr ret_mem1_link_attr,
+		   LmnAtom res_link, LmnLinkAttr res_link_attr)
+{
+  LmnMembraneRef m0 = LMN_PROXY_GET_MEM(LMN_SATOM_GET_LINK(mem0_proxy, 0));
+  LmnMembraneRef m1 = LMN_PROXY_GET_MEM(LMN_SATOM_GET_LINK(mem1_proxy, 0));
+  int judge = inner_mem_cmp(m0, m1);
+  LmnSAtom result;
+  
+  if(judge==0)
+    {
+      result = lmn_mem_newatom(mem, LMN_TRUE_FUNCTOR);
+    }
+  else
+    {
+      result = lmn_mem_newatom(mem, LMN_FALSE_FUNCTOR);
+    }
+
+  lmn_mem_newlink(mem,
+		  LMN_ATOM(result), LMN_ATTR_MAKE_LINK(0), 0,
+		  res_link, res_link_attr,
+		  LMN_ATTR_GET_VALUE(res_link_attr));
+
+  lmn_mem_newlink(mem,
+		  mem0_proxy, mem0_proxy_link_attr, LMN_ATTR_GET_VALUE(mem0_proxy_link_attr),
+		  ret_mem0_link, LMN_ATTR_MAKE_LINK(0),
+		  LMN_ATTR_GET_VALUE(ret_mem0_link_attr));
+
+  lmn_mem_newlink(mem,
+		  mem1_proxy, mem1_proxy_link_attr, LMN_ATTR_GET_VALUE(mem1_proxy_link_attr),
+		  ret_mem1_link, LMN_ATTR_MAKE_LINK(0),
+		  LMN_ATTR_GET_VALUE(ret_mem1_link_attr));
+}
+
 void init_react_rule(void)
 {
   lmn_register_c_fun("cb_react_rule", (void *)cb_react_rule, 4);
   lmn_register_c_fun("cb_react_rule_nd", (void *)cb_react_rule_nd, 4);
   lmn_register_c_fun("cb_mhash", (void *)cb_mhash, 3);
+  lmn_register_c_fun("cb_mem_equals", (void *)cb_mem_equals, 5);
 }
