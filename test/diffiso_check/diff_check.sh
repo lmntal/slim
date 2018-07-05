18ba2deaa7ca23ccc9a387257fd09aea9d35119e
Tsunekawa Yutaro <tsunekawa@ueda.info.waseda.ac.jp>
2018-07-06 02:55:59 +0900

    add test script

---
+++ test/diffiso_check/diff_check.sh
@@ -0,0 +1,19 @@
+#!bin/sh
+if [ -d ./out ]; then
+    rm -rf out
+fi
+mkdir out
+mkdir out/diff
+for file in `ls input/`; do
+    echo -n -e '\e[37;1m'
+    echo -n $file
+    ../../src/slim --hl --nd input/$file > o
+    head -n -2 o > ./out/out_$file.txt
+    diff ./out/out_$file.txt convertedgraph_check/$file.txt > ./out/diff/diff_$file
+    if [ -s ./out/diff/diff_$file ]; then
+	echo -e '\e[31;1m:FAIL'
+    else
+	echo -e '\e[32;1m:PASS'
+    fi
+    rm o
+done
