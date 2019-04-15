08ecb5cca08cbe9e933580183cad655d2f0f3e48
adachi-r <adachi@ueda.info.waseda.ac.jp>
2019-04-15 11:02:45 +0900

    resolved conflict on so.h on 20190415

--- src/loader/so.h
+++ src/loader/so.h
@@ -176,7 +176,7 @@
     unsigned int i;                                                            \
     struct Vector *v;                                                          \
     v = ((LmnMembraneRef)rc->wt(srcmemi))->get_rulesets(); \
-    for (i = 0; i < v->num; i++) {                                             \
+    for (i = 0; i < v->get_num(); i++) {				\
       LmnRuleSetRef cp = new LmnRuleSet(*((LmnRuleSetRef)v->get(i)));      \
       lmn_mem_add_ruleset((LmnMembraneRef)rc->wt(destmemi), cp);               \
     }                                                                          \
