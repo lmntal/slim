256a01d845b933922e280f40ad61269356343ed5
Kazunori Ueda <ueda@ueda.info.waseda.ac.jp>
2019-05-12 23:07:29 +0900

    module io and related files

--- src/ext/init_exts.cpp
+++ src/ext/init_exts.cpp
@@ -42,7 +42,6 @@
 void init_integer(void);
 void init_float(void);
 void init_nlmem(void);
-void init_io(void);
 void init_initial_ruleset(void);
 void init_nd_conf(void);
 void init_time(void);
@@ -59,7 +58,6 @@ void init_builtin_extensions(void)
   init_integer();
   init_float();
   init_nlmem();
-  init_io();
   init_initial_ruleset();
   init_nd_conf();
   init_time();
