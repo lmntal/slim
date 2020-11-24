3dd8390c09f630081828ecb183df5ff6ec8715c7
福井航大 <jre233kei+github@gmail.com>
2020-11-25 02:42:45 +0900

    change: tunum

--- src/vm/task.cpp
+++ src/vm/task.cpp
@@ -556,7 +556,7 @@ static inline BOOL react_ruleset(LmnReactCxtRef rc, LmnMembraneRef mem,
   // printf("%s:%d\n", __FUNCTION__, __LINE__);
   int tnum=1;
   if(parallel)
-    tnum=10;
+    tnum=1;
   int cnt=0;
   react_result = false;
 
