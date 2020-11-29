a054859fc360ff184411b93a1d7cda8e4ee70ffc
福井航大 <jre233kei+github@gmail.com>
2020-11-29 14:21:46 +0900

    change: vector copy

--- src/vm/react_context.hpp
+++ src/vm/react_context.hpp
@@ -106,7 +106,8 @@ struct RuleContext {
 
   RuleContext &operator=(const RuleContext &cxt) {
     // work array を deep copy するように変更
-    this->work_array = new std::vector<LmnRegister>(cxt.work_array.begin(), cxt.work_array.end());
+    //this->work_array = new Vector(cxt.work_array);
+    this->work_array = std::vector<LmnRegister>(std::begin(cxt.work_array), std::end(cxt.work_array));
     //this->work_array = cxt.work_array;
 #ifdef USE_FIRSTCLASS_RULE
     delete this->insertion_events;
