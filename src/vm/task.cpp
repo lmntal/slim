7cf12e6bce77f6f2a540f4818a227d124522dc09
matsumura <matsumura@ueda.info.waseda.ac.jp>
2026-02-21 11:43:12 +0900

    modified:   src/vm/task.cpp

--- src/vm/task.cpp
+++ src/vm/task.cpp
@@ -150,6 +150,7 @@ static BOOL dmem_interpret(LmnReactCxtRef rc, LmnRuleRef rule,
                            LmnRuleInstr instr);
 
 static void mem_oriented_loop(MemReactContext *ctx, LmnMembraneRef mem);
+static void mem_oriented_loop_limited(MemReactContext *ctx, LmnMembraneRef mem);
 
 void Task::lmn_dmem_interpret(LmnReactCxtRef rc, LmnRuleRef rule,
                         LmnRuleInstr instr) {
@@ -235,8 +236,9 @@ void Task::lmn_run(Vector *start_rulesets) {
         lmn_hyperlink_print(mem);
     }
   }
-
-  mem_oriented_loop(mrc.get(), mem);
+  
+  if(!lmn_env.nd && lmn_env.bfs_layer_sync) mem_oriented_loop_limited(mrc.get(), mem);
+  else mem_oriented_loop(mrc.get(), mem);
 
   /** PROFILE FINISH */
   if (lmn_env.profile_level >= 1) {
@@ -285,6 +287,18 @@ void Task::lmn_run(Vector *start_rulesets) {
 
 /** 膜スタックに基づいた通常実行 */
 static void mem_oriented_loop(MemReactContext *ctx, LmnMembraneRef mem) {
+  while (!ctx->memstack_isempty()) {
+    LmnMembraneRef mem = ctx->memstack_peek();//一個前を覗いて
+    if (!Task::react_all_rulesets(ctx, mem)) {//ルールを適用
+      /* ルールが何も適用されなければ膜スタックから先頭を取り除く */
+      ctx->memstack_pop();
+    }
+  }
+}
+
+/** 膜スタックに基づいた通常実行,--limited-stepを通常実行で実行する際にこちらに分岐
+ */
+static void mem_oriented_loop_limited(MemReactContext *ctx, LmnMembraneRef mem) {
   unsigned int maxSteps,nowStep=0;
   maxSteps=lmn_env.depth_limits;
   while (!ctx->memstack_isempty()&&nowStep<maxSteps) {
